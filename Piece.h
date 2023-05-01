54b1a621b62e466d23bdc67c33b69d74bfe08ff9
HarrisonCW <harrison_williams1@baylor.edu>
2023-04-30 22:36:47 -0500

    added function documentation to Piece.h

--- Piece.h
+++ Piece.h
@@ -20,8 +20,26 @@ using namespace std;
 
 class Piece {
 private:
+    /*
+     * description: updates the adjacent Piece pointers in a clockwise
+     *              direction. top->right, right->bottom, bottom->left, and
+     *              left->top
+     * return: void
+     * precondition: all Edge pointers are valid
+     * postcondition: all Edge pointers have been reassigned
+     */
     void rotateEdgesRight();
+
+    /*
+     * description: updates the adjacent Piece pointers in a counter clockwise
+     *              direction. top->left, left->bottom, bottom->right, and
+     *              right->top
+     * return: void
+     * precondition: all Edge pointers are valid
+     * postcondition: all Edge pointers have been reassigned
+     */
     void rotateEdgesLeft();
+
 public:
     bool isSelected;
     int rotation;
@@ -33,13 +51,69 @@ public:
     double width, height;
     double ux, uy;
 
+
+    /*
+     * description: constructs a Piece object and sets default values for
+     *              all variables contained within it
+     * return: none
+     * precondition: all Edges passed as parameters are valid
+     * postcondition: all default variables are set and all Edges are set
+     *                  equal to the appropriate passed parameters
+     */
     Piece(Edge left, Edge top, Edge right, Edge bottom);
+
+    /*
+     * description: rotates the section of the original image
+     * return: void
+     * precondition: Rotate rotation is valid
+     * postcondition: the image is rotated
+     */
     void rotate(Rotate rotation);
+
+    /*
+     * description: determines if two pieces are adjacent to each other in the
+     *              graphical interface
+     * return: bool, true if adjacent
+     * precondition: piece parameter is valid
+     * postcondition: none
+     */
     bool isAdjacent(Piece* piece);
+
+    /*
+     * description: determines if two pieces are oriented in the same direction
+     *              and are adjacent on their appropriate sides to interlock
+     * return: bool, true if able to interlock
+     * precondition: piece parameter is valid
+     * postcondition: none
+     */
     bool canInterlock(Piece* piece);
+
+    /*
+     * description: determines if two pieces are intended to be next to each
+     *              other
+     * return: bool, true if neighbors
+     * precondition: piece parameter is valid
+     * postcondition: none
+     */
     bool areNeighbors(Piece* piece);
+
+    /*
+     * description: determines the x and y coordinates from the original image
+     *              that the specific Piece object will be responsible for
+     * return: void
+     * precondition: none
+     * postcondition: gridx and gridy are set
+     */
     void setGrid();
 
+
+    /*
+     * description: displays the portion of the original image that the Piece
+     *              object pertains to as determined by setGrid
+     * return: void
+     * precondition: texture and screen parameters are valid
+     * postcondition: the portion of the original image is drawn to the screen
+     */
     void drawSelf(PieceTexture* texture, PlotterTexture* screen);
 };
 
