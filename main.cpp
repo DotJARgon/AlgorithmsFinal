ac241d2fb03f0d368ff96dd7c47901e8193364ac
Earthground <tristanbrown377@gmail.com>
2023-05-02 10:46:40 -0500

    MORE PICTURES

--- main.cpp
+++ main.cpp
@@ -13,10 +13,16 @@ int main(int argc, char ** argv)
 {
     //Texture* texture = TextureLoader::test<Texture>(20, 30);
 
-    vector<string> fileName = {"lion.txt", "hogsmeade.txt", "baylorRockWall.txt"};
+    vector<string> fileName = {"lion.txt", "hogsmeade.txt",
+                               "baylorRockWall.txt",
+                               "windows7.txt", "starryNight.txt",
+                               "Texas.txt", "TATE.txt",
+                               "room.txt", "easterIsland.txt"};
     int userInputDif, userInputPic;
-    //TODO Just doing this console input until we get the texture class working for SDL if we do
-    cout << "What difficulty?\n1.) Easy - 2x2\n2.) Medium - 4x4\n3.) Hard - 8x8\n";
+    //TODO Just doing this console input until we get the texture
+    // class working for SDL if we do
+    cout << "What difficulty?\n1.) Easy - 2x2\n2.)"
+            " Medium - 4x4\n3.) Hard - 8x8\n";
 
     cin >> userInputDif;
     switch (userInputDif){
@@ -41,7 +47,8 @@ int main(int argc, char ** argv)
     }
     cout << endl;
     cin >> userInputPic;
-    PieceTexture* puzzle = TextureLoader::loadImage<PieceTexture>(fileName.at(userInputPic - 1));
+    PieceTexture* puzzle = TextureLoader::loadImage<PieceTexture>
+            (fileName.at(userInputPic - 1));
 
     SDL_Plotter* g = new SDL_Plotter(500,1000);
 
@@ -50,13 +57,16 @@ int main(int argc, char ** argv)
     PlotterTexture plotter(g);
 
 
-    Board board(userInputDif, userInputDif, &plotter, puzzle, soundHandler);
+    Board board(userInputDif, userInputDif,
+                &plotter, puzzle, soundHandler);
 
-    long long start = std::chrono::system_clock::now().time_since_epoch().count();
+    long long start = std::chrono::system_clock::
+            now().time_since_epoch().count();
     bool started = false;
 
     while (!g->getQuit() && !board.checkWin()){
-        long long t1 = std::chrono::system_clock::now().time_since_epoch().count();
+        long long t1 = std::chrono::system_clock::
+                now().time_since_epoch().count();
         if(t1 - start > 500000000 && !started) {
             started = true;
             soundHandler->playMusic();
