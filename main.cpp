3d42d53e5c997338c2b3ca5eff33fdfa698d9750
unknown <mcar3550@students.kleinisd.net>
2023-05-04 21:28:04 -0500

    tweaked

--- main.cpp
+++ main.cpp
@@ -42,100 +42,196 @@ int main(int argc, char ** argv)
  // the 3 menus Look at Text.cpp for the implementation All test must be Capital letters only.
 
     //Main Menu
-//    while(!enterHit){
-//        for(int i = 0; i <= 1000; i++){
-//            for(int j = 0; j < 500; j++){
-//                g->plotPixel(i,j,0,0,0);
-//            }
-//        }
-//
-//        displayText("BOOTHS_PUZZLE_LEAGUE", 32, (1000/10) -
-//                                                20, 500/3, 6, 255, 255, 255,*g, a);
-//        displayText("PRESS_ENTER_TO_START", 15, (1000/10) +
-//                                                100, 500/2, 2, 255, 255, 255,*g, a);
-//        if(displayStart){
-//            g->update();
-//            sleep(10);
-//            displayStart = false;
-//        }
-//
-//        if(g->kbhit()){
-//            cout << "you did it dumbass" << endl;
-//            char tmpKey = g->getKey();
-//
-//            if(tmpKey == SDL_SCANCODE_RETURN){
-//                enterHit = true;
-//            }
-//
-//            g->update();
-//        }
-//    }
+    while(!g->getQuit() && !enterHit){
+        for(int i = 0; i < 1000; i++){
+            for(int j = 0; j < 500; j++){
+                plotter.writePixel(i,j,0);
+            }
+        }
+
+        displayText("BOOTHS_PUZZLE_LEAGUE",
+                    32, (1000/10) - 20, 500/3, 6, 255, 255, 255,*g, a);
+        displayText("PRESS_ANY_KEY_TO_START",
+                    15, (1000/10) + 100, 500/2, 2, 255, 255, 255,*g, a);
 
 
+        if(g->kbhit()){
+            enterHit = true;
+        }
 
-//    //Paints Screen Black
-//    for(int i = 0; i <= 1000; i++){
-//        for(int j = 0; j < 500; j++){
-//            g->plotPixel(i,j,0,0,0);
-//        }
-//    }
+        g->update();
+    }
 
+
+    //Paints Screen Black
+    for(int i = 0; i < 1000; i++){
+        for(int j = 0; j < 500; j++){
+            plotter.writePixel(i,j,0);
+        }
+    }
+
+
+    //font size 10x7
+    //multiply by integer values
 
     //Difficulty Select
-//    displayText("SELECT DIFFICULTY", 32, (1000/5) - 20, 500/3, 6, 255,255,255, *g, a);
-//    g->update();
-//        displayText("EASY")
-//        displayText("PRESS 1")
-//        displayText("NORMAL")
-//        displayText("PRESS 2")
-//        displayText("HARD")
-//        displayText("PRESS 3")
+    displayText("SELECT_DIFFICULTY",
+                32, 500 - 28*9, 50, 4, 255,255,255, *g, a);
+    displayText("EASY",
+                32, 28*2, 200, 4, 0,255,0, *g, a);
+    displayText("NORMAL",
+                32, 500-28*3, 200, 4, 255,128,0, *g, a);
+    displayText("HARD",
+                32, 1000-28*6, 200, 4, 255,0,0, *g, a);
 
-    //while(!difficultySelect){
+    displayText("PRESS_1",
+                16, 14*4.5, 300, 2, 255,255,255, *g, a);
+    displayText("PRESS_2",
+                16, 500-14*3.5, 300, 2, 255,255,255, *g, a);
+    displayText("PRESS_3",
+                16, 1000-14*11.5, 300, 2, 255,255,255, *g, a);
 
-//        displayText("SELECT DIFFICULTY", 32, (1000/5) - 20, 500/3, 6, 255,255,255, *g, a);
-//        g->update();
-//        displayText("EASY")
-//        displayText("PRESS 1")
-//        displayText("NORMAL")
-//        displayText("PRESS 2")
-//        displayText("HARD")
-//        displayText("PRESS 3")
-    //}
+    g->update();
+
+    char press;
+
+    sleep(1);
+    while(!g->getQuit()){
+        if(g->kbhit()){
+            press = g->getKey();
+
+            if(press == '1') {
+                userInputDif = 1;
+                break;
+            }
+            else if(press == '2') {
+                userInputDif = 2;
+                break;
+            }
+            else if(press == '3') {
+                userInputDif = 3;
+                break;
+            }
+        }
+
+        g->update();
+    }
+
+    //Paints Screen Black
+    for(int i = 0; i < 1000; i++){
+        for(int j = 0; j < 500; j++){
+            plotter.writePixel(i,j,0);
+        }
+    }
+
+    displayText("SELECT_IMAGE",
+                32, 500 - 28*6, 50, 4, 255,255,255, *g, a);
 
 
-    //Image Select
-//        displayText("SELECT_IMAGE")
-//        displayText("LION")
-//        displayText("PRESS_1")
-//        displayText("HOGSMEADE")
-//        displayText("PRESS_2")
-//        displayText("ROCK_WALL")
-//        displayText("PRESS_3")
-//        displayText("WINDOWS_7")
-//        displayText("PRESS_4")
-//        displayText("STARRY_NIGHT")
-//        displayText("PRESS_5")
-//        displayText("TEXAS")
-//        displayText("PRESS_6")
-//        displayText("TATE")
-//        displayText("PRESS_7")
-//        displayText("ROOM")
-//        displayText("PRESS_8")
-//        displayText("EASTER_ISLAND")
-//        displayText("PRESS_9")
-//        displayText("MUSICIANS")
-//        displayText("PRESS_0")
+    displayText("LION",
+                16, 14*2, 150, 2, 255,255,255, *g, a);
+    displayText("PRESS_1",
+                16, 14, 200, 2, 255,255,255, *g, a);
+
+    displayText("HOGSMEADE",
+                16, 228 - 14*4.5, 150, 2, 255,255,255, *g, a);
+    displayText("PRESS_2",
+                16, 228-14*3.5, 200, 2, 255,255,255, *g, a);
+
+    displayText("ROCK_WALL",
+                16, 428 - 14*4.5, 150, 2, 255,255,255, *g, a);
+    displayText("PRESS_3",
+                16, 428-14*3.5, 200, 2, 255,255,255, *g, a);
+
+    displayText("WINDOWS_7",
+                16, 628 - 14*4.5, 150, 2, 255,255,255, *g, a);
+    displayText("PRESS_4",
+                16, 628-14*3.5, 200, 2, 255,255,255, *g, a);
+
+    displayText("STARRY_NIGHT",
+                16, 828 - 14*6, 150, 2, 255,255,255, *g, a);
+    displayText("PRESS_5",
+                16, 828-14*3.5, 200, 2, 255,255,255, *g, a);
 
 
+    displayText("TEXAS",
+                16, 14*2.5, 350, 2, 255,255,255, *g, a);
+    displayText("PRESS_6",
+                16, 14, 400, 2, 255,255,255, *g, a);
 
+    displayText("TATE",
+                16, 228 - 14*2, 350, 2, 255,255,255, *g, a);
+    displayText("PRESS_7",
+                16, 228-14*3.5, 400, 2, 255,255,255, *g, a);
 
-    //TODO Just doing this console input until we get the texture
-    // class working for SDL if we do
-    cout << "What difficulty?\n1.) Easy - 2x2\n2.)"
-            " Medium - 4x4\n3.) Hard - 8x8\n";
+    displayText("ROOM",
+                16, 428 - 14*2, 350, 2, 255,255,255, *g, a);
+    displayText("PRESS_8",
+                16, 428-14*3.5, 400, 2, 255,255,255, *g, a);
 
-    cin >> userInputDif;
+    displayText("EASTER_ISLAND",
+                16, 628 - 14*6.5, 350, 2, 255,255,255, *g, a);
+    displayText("PRESS_9",
+                16, 628-14*3.5, 400, 2, 255,255,255, *g, a);
+
+    displayText("MUSICIANS",
+                16, 828 - 14*4.5, 350, 2, 255,255,255, *g, a);
+    displayText("PRESS_0",
+                16, 828-14*3.5, 400, 2, 255,255,255, *g, a);
+
+    g->update();
+
+    sleep(1);
+
+    while(!g->getQuit()){
+        if(g->kbhit()){
+            press = g->getKey();
+
+            if(press == '1') {
+                userInputPic = 0;
+                break;
+            }
+            else if(press == '2') {
+                userInputPic = 1;
+                break;
+            }
+            else if(press == '3') {
+                userInputPic = 2;
+                break;
+            }
+            else if(press == '4') {
+                userInputPic = 3;
+                break;
+            }
+            else if(press == '5') {
+                userInputPic = 4;
+                break;
+            }
+            else if(press == '6') {
+                userInputPic = 5;
+                break;
+            }
+            else if(press == '7') {
+                userInputPic = 6;
+                break;
+            }
+            else if(press == '8') {
+                userInputPic = 7;
+                break;
+            }
+            else if(press == '9') {
+                userInputPic = 8;
+                break;
+            }
+            else if(press == '0') {
+                userInputPic = 9;
+                break;
+            }
+        }
+
+        g->update();
+    }
+
     switch (userInputDif){
         case 1:
             userInputDif = 2;
@@ -151,15 +247,8 @@ int main(int argc, char ** argv)
             return 0;
     }
 
-
-    cout << "What picture would you like to use?" << endl;
-    for(int i = 0; i < fileNames.size(); i++){
-        cout << i + 1 << ".) " << fileNames.at(i) << endl;
-    }
-    cout << endl;
-    cin >> userInputPic;
     PieceTexture* puzzle = TextureLoader::loadImage<PieceTexture>
-            (fileNames.at(userInputPic - 1));
+            (fileNames.at(userInputPic));
 
 
     Board board(userInputDif, userInputDif,
