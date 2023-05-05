a03a1d3a300db23fc159f63ef424a1bda7531e07
Pikapower88 <Joshua_Linnett1@baylor.edu>
2023-05-04 23:33:22 -0500

    Fixed Text Spacing and paved way for Textures in Image Select

--- main.cpp
+++ main.cpp
@@ -24,6 +24,9 @@ int main(int argc, char ** argv)
 
 
     bool enterHit = false;
+    bool difficultySelect = false;
+    bool displayStart = true;
+    bool firstRun = true;
 
 
     int a[37][10][7] = {{{0}}};
@@ -50,16 +53,29 @@ int main(int argc, char ** argv)
 
     //Main Menu
     while(!g->getQuit() && !enterHit){
-        for(int i = 0; i < 1000; i++){
-            for(int j = 0; j < 500; j++){
-                plotter.writePixel(i,j,0);
+//        for(int i = 0; i < 1000; i++){
+//            for(int j = 0; j < 500; j++){
+//                plotter.writePixel(i,j,0);
+//            }
+//        }
+
+        for(int i=0 ; i< 32 && firstRun; i++){
+            g->clear();
+            for(int i = 0; i < 1000; i++){
+                for(int j = 0; j < 500; j++){
+                    plotter.writePixel(i,j,0);
+                }
             }
+            displayText("BOOTHS_PUZZLE_LEAGUE", 32, (1000/8) + 42, i*9, 6, 0,0,255, *g, a);
+            g->update();
+            g->Sleep(40);
         }
+        firstRun = false;
 
         displayText("BOOTHS_PUZZLE_LEAGUE",
-                    32, (1000/10) - 20, 500/3, 6, 255, 255, 255,*g, a);
+                    32, (1000/8) + 42, 500/3, 6, 255, 0, 0,*g, a);
         displayText("PRESS_ANY_KEY_TO_START",
-                    15, (1000/10) + 100, 500/2, 2, 255, 255, 255,*g, a);
+                    15, (1000/8) + 180, 500/2, 2, 255, 255, 255,*g, a);
 
 
         if(g->kbhit()){
@@ -137,59 +153,59 @@ int main(int argc, char ** argv)
 
     //draws all of the text to the screen for the menu
     displayText("SELECT_IMAGE",
-                32, 500 - 28*6, 50, 4, 255,255,255, *g, a);
+                32, 500 - 28*6, 25, 4, 255,255,255, *g, a);
 
 
     displayText("LION",
-                16, 14*2, 150, 2, 255,255,255, *g, a);
+                16, 14*2, 75, 2, 255,255,255, *g, a);
     displayText("PRESS_1",
-                16, 14, 200, 2, 255,255,255, *g, a);
+                16, 14, 250, 1, 255,255,255, *g, a);
 
     displayText("HOGSMEADE",
-                16, 228 - 14*4.5, 150, 2, 255,255,255, *g, a);
+                16, 228 - 14*4.5, 75, 2, 255,255,255, *g, a);
     displayText("PRESS_2",
-                16, 228-14*3.5, 200, 2, 255,255,255, *g, a);
+                16, 228-14*3.5, 250, 1, 255,255,255, *g, a);
 
     displayText("ROCK_WALL",
-                16, 428 - 14*4.5, 150, 2, 255,255,255, *g, a);
+                16, 428 - 14*4.5, 75, 2, 255,255,255, *g, a);
     displayText("PRESS_3",
-                16, 428-14*3.5, 200, 2, 255,255,255, *g, a);
+                16, 428-14*3.5, 250, 1, 255,255,255, *g, a);
 
     displayText("WINDOWS_7",
-                16, 628 - 14*4.5, 150, 2, 255,255,255, *g, a);
+                16, 628 - 14*4.5, 75, 2, 255,255,255, *g, a);
     displayText("PRESS_4",
-                16, 628-14*3.5, 200, 2, 255,255,255, *g, a);
+                16, 628-14*3.5, 250, 1, 255,255,255, *g, a);
 
     displayText("STARRY_NIGHT",
-                16, 828 - 14*6, 150, 2, 255,255,255, *g, a);
+                16, 828 - 28, 75, 2, 255,255,255, *g, a);
     displayText("PRESS_5",
-                16, 828-14*3.5, 200, 2, 255,255,255, *g, a);
+                16, 828 + 14, 250, 1, 255,255,255, *g, a);
 
 
     displayText("TEXAS",
-                16, 14*2.5, 350, 2, 255,255,255, *g, a);
+                16, 14*2.5, 300, 2, 255,255,255, *g, a);
     displayText("PRESS_6",
-                16, 14, 400, 2, 255,255,255, *g, a);
+                16, 14, 475, 1, 255,255,255, *g, a);
 
     displayText("TATE",
-                16, 228 - 14*2, 350, 2, 255,255,255, *g, a);
+                16, 228 - 14*2, 300, 2, 255,255,255, *g, a);
     displayText("PRESS_7",
-                16, 228-14*3.5, 400, 2, 255,255,255, *g, a);
+                16, 228-14*3.5, 475, 1, 255,255,255, *g, a);
 
     displayText("ROOM",
-                16, 428 - 14*2, 350, 2, 255,255,255, *g, a);
+                16, 428 - 14*2, 300, 2, 255,255,255, *g, a);
     displayText("PRESS_8",
-                16, 428-14*3.5, 400, 2, 255,255,255, *g, a);
+                16, 428-14*3.5, 475, 1, 255,255,255, *g, a);
 
     displayText("EASTER_ISLAND",
-                16, 628 - 14*6.5, 350, 2, 255,255,255, *g, a);
+                16, 628 - 14*6.5, 300, 2, 255,255,255, *g, a);
     displayText("PRESS_9",
-                16, 628-14*3.5, 400, 2, 255,255,255, *g, a);
+                16, 628-14*3.5, 475, 1, 255,255,255, *g, a);
 
     displayText("MUSICIANS",
-                16, 828 - 14*4.5, 350, 2, 255,255,255, *g, a);
+                16, 828 - 14 , 300, 2, 255,255,255, *g, a);
     displayText("PRESS_0",
-                16, 828-14*3.5, 400, 2, 255,255,255, *g, a);
+                16, 828 + 14, 475, 1, 255,255,255, *g, a);
 
     g->update();
 
