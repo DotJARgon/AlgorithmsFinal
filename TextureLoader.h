d32fca84ef2348de112fecfdbfcb4810689ad6f5
unknown <mcar3550@students.kleinisd.net>
2023-05-04 21:48:13 -0500

    pruning unnecessary code

--- TextureLoader.h
+++ TextureLoader.h
@@ -18,7 +18,14 @@ using namespace std;
 
 class TextureLoader {
 public:
-    //class must be of type TextureBuffer
+    /*
+    * description: This function loads TextureBuffer types from a file given as
+     *             an input
+    * return: void
+    * precondition: The template type is of TextureBuffer, the filename passed
+     *              is a file that exists
+    * postcondition: A new TextureBuffer is created
+    */
     template<class T>
     static T* loadImage(string filename) {
         ifstream in;
@@ -37,63 +44,6 @@ public:
             int r, g, b;
             in >> r >> g >> b;
             int argb = (r << 16) | (g << 8) | b;
-            data[i++] = argb;
-        }
-
-        in.close();
-
-        T* t = new T(width, height);
-        reinterpret_cast<TextureBuffer*>(t)->replaceData(data);
-        return t;
-    }
-
-    template<class T>
-    static T* test(int width, int height) {
-        int* data = new int[width*height];
-        for(int i = 0; i < width; i++) {
-
-            for(int j = 0; j < height; j++) {
-                data[j * width + i] = ((i+j)%2)*255;
-                //cout << data[j * width + i] << endl;
-            }
-        }
-
-        T* t = new T(width, height);
-        reinterpret_cast<TextureBuffer*>(t)->replaceData(data);
-        return t;
-    }
-    template<class T>
-    static T* test2(int width, int height) {
-        int* data = new int[width*height];
-        for(int i = 0; i < width; i++) {
-
-            for(int j = 0; j < height; j++) {
-                data[j * width + i] = (((i/2+j/2))%2)*255;
-                //cout << data[j * width + i] << endl;
-            }
-        }
-
-        T* t = new T(width, height);
-        reinterpret_cast<TextureBuffer*>(t)->replaceData(data);
-        return t;
-    }
-
-    template<class T>
-    static T* loadAtlas(string filename) {
-        ifstream in;
-        in.open(filename);
-
-        if(!in) return nullptr;
-        int width, height;
-        in >> width >> height;
-        in.get();
-
-        int* data = new int[width*height];
-
-        string line;
-        int i = 0;
-        while(getline(in, line)) {
-            int argb = stoi(line);
             data[i++] = argb;
         }
 
